4bcfce4e96340be8b2ddee175b523740b371d73b
maia-tarapipipi <tarapipipim@gmail.com>
2023-07-17 17:32:06 +1200

    updates pictures for sliders

--- client/components/Pages/Home.tsx
+++ client/components/Pages/Home.tsx
@@ -3,24 +3,39 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
 function Home() {
-  const NoGallery = [
-    'anzac-bay-1.jpeg',
-    'anzac-bay-2.jpeg',
-    'kainga-1.jpeg',
-    'kainga-2.jpeg',
-    'kainga-3.jpeg',
-    'kainga-4.jpg',
-    'poleroid-1.jpeg',
+  const NoHeaKoeGallery = [
+    'campus-1.jpeg',
+    'campus-2.jpeg',
+    'campus-3.jpeg',
+    'campus-4.jpeg',
+    'campus-5.jpeg',
+    'campus-6.jpeg',
+    'campus-7.jpeg',
+    'campus-8.jpeg',
+    'campus-9.jpeg',
+    'campus-10.jpeg',
+    'campus-11.jpeg',
+    'campus-12.jpeg',
+    'campus-13.jpeg',
+    'campus-14.jpeg',
+    'campus-15.jpeg',
+    'campus-16.jpeg',
+    'campus-17.jpeg',
+    'campus-18.jpeg',
+    'campus-19.jpeg',
+    'campus-20.jpeg',
   ]
 
-  const KaiGallery = [
-    'anzac-bay-1.jpeg',
-    'anzac-bay-2.jpeg',
+  const KaingaGallery = [
     'kainga-1.jpeg',
     'kainga-2.jpeg',
     'kainga-3.jpeg',
-    'kainga-4.jpg',
-    'poleroid-1.jpeg',
+    'kainga-4.jpeg',
+    'kainga-5.jpeg',
+    'kainga-6.jpeg',
+    'photocopy-1.jpg',
+    'photocopy-2.jpg',
+    'photocopy-3.jpg',
   ]
 
   const settings = {
@@ -36,12 +51,15 @@ function Home() {
 
   return (
     <div>
+      <button>
+        
+      </button>
       <div>
         <Slider {...settings} className="my-12">
-          {NoGallery.map((path, index) => (
+          {NoHeaKoeGallery.map((path, index) => (
             <div key={index}>
               <img
-                src={`/NoGallery/${path}`}
+                src={`/NoHeaKoeGallery/${path}`}
                 alt={`${path}`}
                 className="h-96"
               />
@@ -52,10 +70,10 @@ function Home() {
 
       <div>
         <Slider {...settings} className="my-12">
-          {KaiGallery.map((path, index) => (
+          {KaingaGallery.map((path, index) => (
             <div key={index}>
               <img
-                src={`/NoGallery/${path}`}
+                src={`/KaingaGallery/${path}`}
                 alt={`${path}`}
                 className="h-96"
               />
