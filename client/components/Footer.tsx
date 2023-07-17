5b8ad8650ac40f267754220406a3852fd321d76f
maia-tarapipipi <tarapipipim@gmail.com>
2023-07-17 13:14:37 +1200

    correct icons imported into document

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,15 +1,16 @@
-import { LinkedinShareButton, LinkedinIcon } from 'react-share'
+import { LinkedinShareButton } from 'react-share'
 import { IoLogoLinkedin } from 'react-icons/io5'
+import { FaPaperPlane } from 'react-icons/fa'
 
 function Footer() {
   return (
-    <div>
+    <div className="w-70 flex items-center justify-evenly">
       <LinkedinShareButton
         url={'https://www.linkedin.com/in/maia-tarapipipi-a08ab4282/'}
-        className="w-full flex items-center justify-center"
       >
-        <IoLogoLinkedin size={40} className="text-center" />
+        <IoLogoLinkedin size={40} />
       </LinkedinShareButton>
+      <FaPaperPlane size={40} />
     </div>
   )
 }
