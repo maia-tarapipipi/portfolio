baab76a55915df6276d9394087be1f25883c13c7
maia-tarapipipi <tarapipipim@gmail.com>
2023-07-17 14:30:46 +1200

    social icons up and running

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,18 +1,34 @@
-import { LinkedinShareButton } from 'react-share'
+import { EmailShareButton } from 'react-share'
 import { IoLogoLinkedin } from 'react-icons/io5'
 import { FaPaperPlane } from 'react-icons/fa'
+import { BsGithub } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
-    <div className="w-70 flex items-center justify-evenly">
-      <LinkedinShareButton
-        url={'https://www.linkedin.com/in/maia-tarapipipi-a08ab4282/'}
-      >
-        <IoLogoLinkedin size={40} />
-      </LinkedinShareButton>
-      <FaPaperPlane size={40} />
+    <div>
+      <div className="w-70 flex items-center justify-center">
+        <Link
+          to="https://www.linkedin.com/in/maia-tarapipipi-a08ab4282/"
+          target="_blank"
+        >
+          <IoLogoLinkedin size={40} className="mx-5" />
+        </Link>
+
+        <EmailShareButton url={'tarapipipim'}>
+          <FaPaperPlane size={40} className="mx-5" />
+        </EmailShareButton>
+
+        <Link to="https://github.com/maia-tarapipipi" target="_blank">
+          <BsGithub size={40} className="mx-5" />
+        </Link>
+      </div>
+      <div className="w-70 flex items-center justify-evenly">
+        <Link to="/">return to top</Link>
+      </div>
     </div>
   )
 }
 
 export default Footer
+// /Users/maiatarapipipi/workspace/portfolio/public/maia-tarapipipi-CV-2023.pdf
