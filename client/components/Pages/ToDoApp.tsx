18a1af2f4e10b591823cdbc0d0b4e72bf735263b
maia-tarapipipi <tarapipipim@gmail.com>
2023-07-25 11:05:23 +1200

    refactors to-do app

--- client/components/Pages/ToDoApp.tsx
+++ client/components/Pages/ToDoApp.tsx
@@ -1,37 +1,68 @@
 import { BsGithub } from 'react-icons/bs'
+import ReactPlayer from 'react-player'
 import { Link } from 'react-router-dom'
 
 function ToDoApp() {
   return (
     <div className="px-20 pb-10">
       <h1 className="text-2xl">To Do App</h1>
-      <Link to="https://github.com/maia-tarapipipi/to-do-app" target="_blank">
-        Visit the Repo!
-        <BsGithub size={40} className="mx-5 hover:fill-slate-500" />
-      </Link>
-      <p>
-        Welcome to To Do App! The aim for this project was to create a simple
-        way of keeping track of the things you need to do!
-        <img
-          src="/To-do-app-screenshot.png"
-          alt="to-do app"
-          className="w-1/4"
-        />
-        to-do app which users can:
-        <ul className="list-disc pl-10">
-          <li>Create a to-do which is stored on the database</li>
-          <li>Store global state with Redux</li>
-        </ul>
-      </p>
+      <div className="flex flex-row">
+        <div>
+          <p>
+            Welcome to To Do App! My aim with this project is to provide a
+            simple and effective way for you to keep track of all your tasks and
+            stay organized!{' '}
+          </p>
+          <p>Features:</p>
+          <ul className="list-disc pl-10">
+            <li>
+              Create To-Do: Easily create and add to-dos, which are securely
+              stored in the database. This feature ensures that your tasks are
+              saved and accessible across sessions.
+            </li>
+            <li>
+              Global State Management with Redux: To Do App leverages the power
+              of Redux to manage global state efficiently. This enables seamless
+              data synchronization and consistent user experiences throughout
+              the app.
+            </li>
+          </ul>
+        </div>
+        <div className="text-lg flex justify-center hover:text-slate-500 w-1/4 items-center">
+          <Link
+            to="https://github.com/maia-tarapipipi/to-do-app"
+            target="_blank"
+          >
+            <BsGithub size={80} className="mx-5 hover:fill-slate-500" />
+            Visit the Repo!
+          </Link>
+        </div>
+      </div>
 
-      <p>
-        In the future I plan:
-        <ul className="list-disc pl-10">
-          <li>to personalize the styling</li>
-          <li>have it fully deployed</li>
-          <li>Delete a to-do</li>
-        </ul>
-      </p>
+      <h1 className="text-center text-lg">Demo</h1>
+      <div className="flex justify-center">
+        <ReactPlayer playing url="https://youtu.be/vZRTen1HOsE" />
+      </div>
+
+      <p>Future Plans:</p>
+
+      <ul className="list-disc pl-10">
+        <li>
+          Personalized Styling: We are working on allowing users to personalize
+          the app&apos;s styling to match their preferences. With custom themes
+          and styling options, you can make To Do App truly your own.
+        </li>
+
+        <li>
+          Delete a To-Do: We understand that task management also involves the
+          ability to remove completed or unnecessary to-dos. Soon, you&apos;ll
+          be able to easily delete tasks with just a few clicks.
+        </li>
+        <li>
+          Completed To-dos: See a list of your completed to-dos to see how your
+          tracking with your task progress in a separate page{' '}
+        </li>
+      </ul>
     </div>
   )
 }
